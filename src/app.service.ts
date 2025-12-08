757ef20bc8df21af459d9e0c519cfd3a2fdaa38e
boomcs331 <tanapong.wa@tfac.or.th>
2025-12-09 04:47:00 +0700

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly Logger = new Logger(AppService.name);
   getHello(): string {
+      this.Logger.warn(`TEST`);
     return 'Hello World!';
   }
 }
